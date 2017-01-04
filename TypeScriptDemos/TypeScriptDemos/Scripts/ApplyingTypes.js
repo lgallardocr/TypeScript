9044e44e17771fa346530446f56b2038baddd9bd
lgallardocr <luisrgardo@gmail.com>
2017-01-03 22:54:58 -0600

    Applying Types - Practice

---
+++ TypeScriptDemos/TypeScriptDemos/Scripts/ApplyingTypes.js
@@ -0,0 +1,35 @@
+var ApplyingTypes;
+(function (ApplyingTypes) {
+    //  Any
+    var data;
+    var info;
+    var doSomething = function (args) {
+        return args;
+    };
+    var x = doSomething(5);
+    //  primitives
+    var age = 22;
+    var score = 35.36;
+    var rating = 99.99;
+    var hasData = true;
+    var isReady = true;
+    var isBald = function () { return "yes"; };
+    var hasHair = !isBald();
+    var firstName = "Luis";
+    var lastName = "Gallardo";
+    //  string array
+    function getArrayLenght(x) {
+        var len = x.length;
+        return len;
+    }
+    var names = ["Luis", "Gaby", "Allan", "German", "Karla", "Jose"];
+    var firstPerson = names[0];
+    console.log(getArrayLenght(names));
+    //  nulls
+    var guitarSales = null;
+    var animal = null;
+    var orderDate = null;
+    //  undefined
+    var quantity;
+    var company = undefined;
+})(ApplyingTypes || (ApplyingTypes = {}));
---
+++ TypeScriptDemos/TypeScriptDemos/Scripts/ApplyingTypes.ts
@@ -0,0 +1,43 @@
+﻿module ApplyingTypes {
+
+    //  Any
+    var data: any;
+    var info;
+    var doSomething = function (args) {
+        return args;
+    };
+    var x = doSomething(5);
+
+    //  primitives
+    var age: number = 22;
+    var score: number = 35.36;
+    var rating = 99.99;
+
+    var hasData: boolean = true;
+    var isReady = true;
+    var isBald = function () { return "yes"; }
+    var hasHair = !isBald();
+
+    var firstName: string = "Luis";
+    var lastName: string = "Gallardo";
+
+    //  string array
+    function getArrayLenght(x: string[]) {
+        var len: number = x.length;
+        return len;
+    }
+
+    var names: string[] = ["Luis", "Gaby", "Allan", "German", "Karla", "Jose"];
+    var firstPerson = names[0];
+    console.log(getArrayLenght(names));
+
+    //  nulls
+    var guitarSales: any = null;
+    var animal = null;
+    var orderDate: Date = null;
+
+    //  undefined
+    var quantity: number;
+    var company = undefined;
+
+}
