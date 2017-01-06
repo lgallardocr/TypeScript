724fbe18b19e0aecb450996124cfca7f3c85775b
lgallardocr <luisrgardo@gmail.com>
2017-01-05 22:28:54 -0600

    Pluralsight - Objects

---
+++ TypeScriptDemos/TypeScriptDemos/Scripts/Objects.js
@@ -0,0 +1,35 @@
+var Objects;
+(function (Objects) {
+    //  Object
+    var points1 = { x: 10, y: 20 };
+    var x = points1.x;
+    var points2 = {};
+    points2 = { x: 10, y: 20 };
+    var points3 = { x: 1 };
+    var rectangle = {
+        h: 10,
+        w: 20,
+        calcArea: function () {
+            return this.h * this.w;
+        }
+    };
+    console.log(' Rec Area = ' + rectangle.calcArea());
+    //  Functions
+    var squareIt1 = function (x) {
+        return x * x;
+    };
+    var val1 = squareIt1(2);
+    console.log("SquaredIt1 = " + val1);
+    var val1 = squareIt1(8);
+    console.log("SquaredIt1 = " + val1);
+    var squareIt = function (rect) {
+        if (rect.w === undefined) {
+            return rect.h * rect.h;
+        }
+        return rect.h * rect.w;
+    };
+    var sq1 = squareIt({ h: 10 });
+    console.log("SquareIt = " + sq1);
+    var sq2 = squareIt({ h: 10, w: 20 });
+    console.log("SquareIt = " + sq2);
+})(Objects || (Objects = {}));
